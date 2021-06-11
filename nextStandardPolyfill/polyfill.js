21fab4c8fbccc9e89434455de05780ebd0d52b3a
Error4767 <2303184060@qq.com>
2021-06-11 12:44:55 +0800

    add ecmascript stage3 proposal features support

---
+++ nextStandardPolyfill/polyfill.js
@@ -0,0 +1,6 @@
+import "./atomic-wait-async.js";
+import "./relative-indexing-method.js";
+import "./error-cause.js";
+import "./accessible-object-hasownproperty.js";
+import { Temporal } from 'proposal-temporal/dist/index.umd.js';
+globalThis.Temporal = Temporal;
