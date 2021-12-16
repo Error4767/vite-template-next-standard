2d875da2a3367425ed0df90edeea582ce0b38d25
Error4767 <2303184060@qq.com>
2021-12-16 16:25:43 +0800

    add Array.prototype.groupBy support, update packages

--- nextStandardPolyfill/polyfill.js
+++ nextStandardPolyfill/polyfill.js
@@ -4,4 +4,5 @@ import "./error-cause.js";
 import "./accessible-object-hasownproperty.js";
 import "./array-find-from-last";
 import { Temporal } from 'proposal-temporal/dist/index.umd.js';
+import  "core-js/actual/array/group-by";
 globalThis.Temporal = Temporal;
