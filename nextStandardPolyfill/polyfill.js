08cca8b021302ac33c439708ae92874fadaaad92
error4767 <2303184060@qq.com>
2022-11-12 16:03:31 +0800

    update add depenpencies, delete [array-find-from-last, relative-indexing-method] proposals polyfill

--- nextStandardPolyfill/polyfill.js
+++ nextStandardPolyfill/polyfill.js
@@ -1,7 +1,5 @@
 import "./atomic-wait-async.js";
-import "./relative-indexing-method.js";
 import "./accessible-object-hasownproperty.js";
-import "./array-find-from-last";
 import { Temporal, toTemporalInstant  } from '@js-temporal/polyfill';
 import  "core-js/actual/array/group-by";
 import "./proposal-change-array-by-copy.js";
