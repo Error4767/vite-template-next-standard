4559b52c9c4bb547164dc90891de3f49126586ec
error4767 <2303184060@qq.com>
2023-01-30 17:15:45 +0800

    delete Object.hasOwn polyfill, add Array.fromAsync polyfill, update dependencies

--- nextStandardPolyfill/polyfill.js
+++ nextStandardPolyfill/polyfill.js
@@ -1,8 +1,9 @@
 import "./atomic-wait-async.js";
-import "./accessible-object-hasownproperty.js";
 import { Temporal, toTemporalInstant  } from '@js-temporal/polyfill';
 import  "core-js/actual/array/group-by";
 import "./proposal-change-array-by-copy.js";
+import fromAsync from "array-from-async";
 
 globalThis.Temporal = Temporal;
 Date.prototype.toTemporalInstant = toTemporalInstant;
+Array.fromAsync = fromAsync;
