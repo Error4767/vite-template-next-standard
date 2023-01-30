a7442e13296a52797c93729d4323400fe43484cf
error4767 <2303184060@qq.com>
2023-01-30 17:28:09 +0800

    support ['set-methods', 'iterator-helper', is-well-formed', 'to-well-formed']

--- nextStandardPolyfill/polyfill.js
+++ nextStandardPolyfill/polyfill.js
@@ -3,7 +3,13 @@ import { Temporal, toTemporalInstant  } from '@js-temporal/polyfill';
 import  "core-js/actual/array/group-by";
 import "./proposal-change-array-by-copy.js";
 import fromAsync from "array-from-async";
+import '@rauschma/set-methods-polyfill/install';
+import '@rauschma/iterator-helpers-polyfill/install';
+import isWellFormed from 'string.prototype.iswellformed';
+import toWellFormed  from 'string.prototype.towellformed';
 
 globalThis.Temporal = Temporal;
 Date.prototype.toTemporalInstant = toTemporalInstant;
 Array.fromAsync = fromAsync;
+globalThis.isWellFormed = isWellFormed;
+globalThis.toWellFormed = toWellFormed;
