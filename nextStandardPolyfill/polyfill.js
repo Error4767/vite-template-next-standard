2d11d1c9f0447a7543a29a1b1ed58c2d2c68af7b
2303184060 <2303184060@qq.com>
2024-04-17 01:59:28 +0000

    add proposal-arraybuffer-base64, delete [set-methods-polyfill, Promise.withResolvers, Well-Formed Unicode Strings, Array Grouping]

--- nextStandardPolyfill/polyfill.js
+++ nextStandardPolyfill/polyfill.js
@@ -1,13 +1,7 @@
-import { Temporal, toTemporalInstant  } from '@js-temporal/polyfill';
-import  "core-js/actual/array/group-by";
-import '@rauschma/set-methods-polyfill/install';
+import { Temporal, Intl, toTemporalInstant } from '@js-temporal/polyfill';
 import '@rauschma/iterator-helpers-polyfill/install';
-import isWellFormed from 'string.prototype.iswellformed';
-import toWellFormed  from 'string.prototype.towellformed';
 
-import "./promise-with-resolvers";
+import "./proposal-arraybuffer-base64/polyfill-install.js";
 
 globalThis.Temporal = Temporal;
 Date.prototype.toTemporalInstant = toTemporalInstant;
-globalThis.isWellFormed = isWellFormed;
-globalThis.toWellFormed = toWellFormed;
--- nextStandardPolyfill/promise-with-resolvers.js
+++
@@ -1,9 +0,0 @@
-Promise.withResolvers = ()=> {
-  let resolve;
-  let reject;
-  const promise = new Promise((_resolve, _reject) => {
-      resolve = _resolve;
-      reject = _reject;
-  });
-  return { resolve, reject, promise };
-}
