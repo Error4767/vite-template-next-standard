bd831e29648fa3bd6eb39ed12a9bb8fe69f6c831
2303184060 <2303184060@qq.com>
2024-08-20 09:41:45 +0000

    add Promise.try

--- nextStandardPolyfill/polyfill.js
+++ nextStandardPolyfill/polyfill.js
@@ -3,5 +3,8 @@ import '@rauschma/iterator-helpers-polyfill/install';
 
 import "./proposal-arraybuffer-base64/polyfill-install.js";
 
+import promiseTry from "promise.try";
+promiseTry.shim();
+
 globalThis.Temporal = Temporal;
 Date.prototype.toTemporalInstant = toTemporalInstant;
